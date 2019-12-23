cbfe1cd68ee94b1090b88c35e90ee67e69865d78
Flor <gabrielggff25@gmail.com>
2019-12-23 13:21:26 -0300

    Added arguments input

--- buildAndReleaseTask/test/main.js
+++ buildAndReleaseTask/test/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const [arg1, arg2, arg3] = process.argv.slice(2);
+
+console.log("Arguments", arg1, arg2, arg3)
+testArgv(arg1, "test");
+testArgv(arg2, "test.js");
+testArgv(arg3, "4500");
+
+function testArgv(argv, expected){
+    if(argv !== expected) console.log("##vso[task.logissue type=error]Error testing arguments " + argv);
+    else console.log("Argument OK " + argv);
+}
